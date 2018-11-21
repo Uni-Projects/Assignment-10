34c28ffa7f2e24ce737fbce8aabc4843ef47c0ff
Paul2803k <44923416+Paul2803k@users.noreply.github.com>
2018-11-21 09:41:40 +0100

    Finished part 1, it works fine but i think it can be better..

--- main.cpp
+++ main.cpp
@@ -3,22 +3,24 @@
 
 
 using namespace std;
-const int MAX = 10 ;
+int a [] = {2,14,30,29,42,30,11,-5,0,51};
+const int MAX = sizeof(a)/sizeof(a[0]);
 
-void swapfiets (int (&a)[MAX], int i)
+void swap (int a[], int first,int second)
 {
     //preconditions:
-    assert((i-1)/2 >= 0 && (i-1)/2 < MAX);
-    //postconditions: this function swap the parent with its children.
+    assert(first >= 0 && first < MAX);
+    assert(second >= 0 && second < MAX);
+    //postconditions: this function swaps the 2 elements of the array/vector.
 
-    int temp = 0;
-    temp = a[i];
+    int temp;
 
-    a [i] = a[(i-1)/2];
-    a[(i-1)/2] = temp;
+    temp = a[first];
+    a[first] = a [second];
+    a[second] = temp;
 }
 
-void push_up(int (&a)[MAX], int i)
+void push_up(int a[], int i)
 {
     //preconditions:
     assert(i >= 0 && i < MAX);
@@ -26,12 +28,12 @@ void push_up(int (&a)[MAX], int i)
 
     while (a[i] > a[(i-1)/2])
     {
-        swapfiets (a, i);
+        swap (a, i , (i-1)/2);
         i = (i-1)/2;
     }
 }
 
-void build_heap(int (&a)[MAX])
+void build_heap(int a[])
 {
     //preconditions:
     assert(true);
@@ -41,62 +43,57 @@ void build_heap(int (&a)[MAX])
     {
         push_up(a,i);
     }
-
 }
 
-void swap (int (&a)[MAX], int first,int second)
+void push_down(int a[], int MAX_EL)
 {
-    int temp;
-
-    temp = a[first];
-
-    a[first] = a [second];
-
-    a[second] = temp;
-}
-
-void push_down(int (&a)[MAX], int i,int& counter)
-{
-    swap (a , 0 , MAX - 1 - counter );
-    cout <<counter << " last element: " << a[ MAX - 1 - counter] << endl;
-    counter ++ ;
-
-
-    while ((2*i+1 <(MAX - counter )) && (a[i] < a[2*i+1] || a[i] < a[2*i+2]) )
+    while (MAX_EL > 0)
     {
-        if (a[i] < a[2*i+1])
-        {
-            swap (a, i , 2*i + 1);
-            i = 2*i + 1;
-        }
-        else
-        {
-            swap (a, i , 2*i + 2);
-            i = 2*i + 2;
-        }
+      int i = 0;
+      while (2*i+1 < MAX_EL)
+      {
+          if (a[i] < a[2*i+1])
+          {
+              swap (a, i , 2*i + 1);
+              i = 2*i + 1;
+          }
+          else if (a[i] < a[2*i+2] && 2*i+2 < MAX_EL)
+          {
+              swap (a, i , 2*i + 2);
+              i = 2*i + 2;
+          }
+          else
+            i++;
+       }
+       MAX_EL--;
     }
-
 }
 
-void pick_heap(int (&a)[MAX])
+void pick_heap(int a[])
 {
-    int counter = 0 ;
-    for (int i = 0 ; i < MAX - counter ; i = i)
+    int MAX_EL = MAX;
+
+    for (int i = 0; i < MAX ; i++)
     {
-        push_down(a,i, counter);
+        swap (a,0, MAX_EL-1);
+        MAX_EL--;
+        push_down(a,MAX_EL);
     }
 }
 
 int main()
 {
-    int a [MAX] = {2,14,30,29,42,30,11,-5,0,51};
     build_heap(a);
+
+    cout << "Heaped array: " << endl ;
 
     for (int i = 0 ; i < MAX ; i++)
     {
         cout << a[i] << " ";
     }
     cout << endl ;
+
+    cout << "Sorted array: " << endl ;
 
     pick_heap(a);
 
